ca0aef949e1bca60e7f891c0a30eadf6c30d7566
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-11-09 14:32:43 -0500

    feat: remove .value in preformat

--- preformat.js
+++ preformat.js
@@ -3,7 +3,8 @@ fs.readFile('tokens/tokens.json', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  var result = data.replace(/fontWeights/g, 'fontWeight').replace(/}"/g, '.value}"').replace(/}, /g, '.value}, ');
+  var result = data.replace(/fontWeights/g, 'fontWeight');
+  // .replace(/}"/g, '.value}"').replace(/}, /g, '.value}, ');
 
   fs.writeFile('tokens/tokens.json', result, 'utf8', function (err) {
      if (err) return console.log(err);
