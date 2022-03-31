510af2956bd08148ed6faf501ce4355b4d68e879
Mark Anthony Cianfrani <markacianf@gmail.com>
2022-03-31 08:36:52 -0400

    algolia is magic

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -19,5 +19,11 @@ module.exports = {
 				link: '/components'
 			}
 		],
-	      },
+		algolia: {
+			appId: "6VY3VEO12D",
+			apiKey: '2ede37922e51fc959e70c62b5ccc82cd',
+			indexName: 'astro-design-tokens'
+		      }
+	      }
+
 };
