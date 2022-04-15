ac5b4c91e8d775eaa037bc4a2f4bcf566d8ced42
Mark Anthony Cianfrani <markacianf@gmail.com>
2022-04-15 11:19:30 -0400

    wip

--- preformat.js
+++ preformat.js
@@ -1,4 +1,6 @@
-var fs = require('fs')
+var fs = require('fs').promises
+const _ = require('lodash')
+
 fs.readFile('tokens/tokens.json', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
@@ -11,17 +13,49 @@ fs.readFile('tokens/tokens.json', 'utf8', function (err,data) {
   });
 });
 
+let light = {}
+
+const parseLight = async() => {
+  const data = await fs.readFile('tokens/tokens.light.json')
+  return JSON.parse(data)
+}
+
+const parseDark = async() => {
+  const data = await fs.readFile('tokens/tokens.json')
+  return JSON.parse(data)
+}
+
+
+const init = async() => {
+  let light = await parseLight()
+  let dark = await parseDark()
+
+const data =  _.defaultsDeep(light, dark)
+console.log('data', data);
+
+fs.writeFile('tokens/test.json', JSON.stringify(data), 'utf8', function (err) {
+  if (err) return console.log(err);
+});
+
+
+}
+
+
+init()
+
 fs.readFile('tokens/tokens.light.json', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
   var result = data.replace(/fontWeights/g, 'fontWeight');
+  light = result
   // .replace(/}"/g, '.value}"').replace(/}, /g, '.value}, ');
 
   fs.writeFile('tokens/tokens.light.json', result, 'utf8', function (err) {
      if (err) return console.log(err);
   });
 });
+console.log('light', light);
 
 // fs.readFile('tokens/tokens-dark.json', 'utf8', function (err,data) {
 //   if (err) {
