6834a14252fcb828dd3d63b8a234318cf79482fb
Mark Anthony Cianfrani <markacianf@gmail.com>
2023-02-17 14:24:40 -0500

    build

--- run.js
+++ run.js
@@ -14,7 +14,7 @@ const {exec} = require('child_process');
 // Core
 exec('node_modules/.bin/token-transformer data/tokens.json tokens/base.reference.json reference --expandTypography --preserveRawValue')
 exec('node_modules/.bin/token-transformer data/tokens.json tokens/base.system.json reference,system reference --expandTypography --preserveRawValue')
-exec('node_modules/.bin/token-transformer data/tokens.json tokens/base.component.json reference,system,components,input reference,system --expandTypography --preserveRawValue')
+exec('node_modules/.bin/token-transformer data/tokens.json tokens/base.component.json reference,system,components,input,button reference,system --expandTypography --preserveRawValue')
 
 // Light Theme
 exec('node_modules/.bin/token-transformer data/tokens.json tokens/theme/light.json reference,system,components,theme-light reference,system,components --expandTypography --preserveRawValue')
